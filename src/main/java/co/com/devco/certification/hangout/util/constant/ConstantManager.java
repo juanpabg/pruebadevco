2109ffb64d9180292626b8c8a4bba8fb30e171e4
juanpabg <48089868+juanpabg@users.noreply.github.com>
2019-02-28 06:49:49 -0500

    Update ConstantManager.java
    mejora

--- src/main/java/co/com/devco/certification/hangout/util/constant/ConstantManager.java
+++ src/main/java/co/com/devco/certification/hangout/util/constant/ConstantManager.java
@@ -8,7 +8,7 @@ public class ConstantManager {
 	public static final String PROPERTIES = "properties";
 	public static final String BACK_SLASH = "/";
 	public static final String URL_FRONT = "UrlFront";
-	public static final String SVE = "sve";
+	public static final String USER = "user";
 	public static final String CHROME = "chrome";
 	public static final String IFRAME ="iframe";
 	private ConstantManager() {
